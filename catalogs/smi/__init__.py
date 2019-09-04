1301b77b79d5c47873a059bbb4644a892bed4544
Garrett <bisc8233@gmail.com>
2019-09-04 15:38:41 -0400

    added all of the catalog entrypoints

---
+++ catalogs/smi/__init__.py
@@ -0,0 +1,8 @@
+from databroker import Broker
+import yaml
+
+def load_config(filename):
+    with open(filename) as f:
+        return yaml.load(f, Loader=getattr(yaml, 'FullLoader', yaml.Loader))
+
+catalog = Broker.from_config(load_config('smi.yml'))
