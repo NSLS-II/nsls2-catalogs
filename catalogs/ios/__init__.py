b037b90fdd98be65f1244d9dc2fd30d212e683ae
Garrett Bischof <gbischof@bnl.gov>
2019-09-11 15:58:40 -0400

    some updates

--- catalogs/ios/__init__.py
+++ catalogs/ios/__init__.py
@@ -1,4 +1,4 @@
-from databroker import Broker
+from databroker.v1 import from_config
 from .. import load_config
 
-catalog = Broker.from_config(load_config('ios/ios.yml'))
+catalog = from_config(load_config('ios/ios.yml')).v2
