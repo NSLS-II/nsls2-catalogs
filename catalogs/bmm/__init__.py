dbddb2258500aa9da1e088ac234e74402737287a
Garrett Bischof <gbischof@bnl.gov>
2020-03-18 17:17:54 -0400

    added name

--- catalogs/bmm/__init__.py
+++ catalogs/bmm/__init__.py
@@ -5,3 +5,6 @@ from .. import load_config
 v0_catalog = Broker.from_config(load_config('bmm/bmm.yml'))
 v1_catalog = from_config(load_config('bmm/bmm.yml'))
 catalog = from_config(load_config('bmm/bmm.yml')).v2
+v0_central = Broker.from_config(load_config_central('bmm/bmm.yml', 'BMM'))
+v1_central = from_config(load_config_central('bmm/bmm.yml', 'BMM'))
+central = from_config(load_config_central('bmm/bmm.yml', 'BMM')).v2
