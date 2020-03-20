0b97672098ad32d3ce93357e4f9232e372f0e9c3
gbischof <gbischof@bnl.gov>
2020-03-20 13:03:44 -0400

    updated all of the files

--- catalogs/rsoxs/__init__.py
+++ catalogs/rsoxs/__init__.py
@@ -1,11 +1,8 @@
 from databroker.v1 import from_config
 from databroker.v0 import Broker
-from .. import load_config, load_config_central
+from .. import load_config
 
 name = 'rsoxs'
 v0_catalog = Broker.from_config(load_config(f'{name}/{name}.yml'))
 v1_catalog = from_config(load_config(f'{name}/{name}.yml'))
 catalog = from_config(load_config(f'{name}/{name}.yml')).v2
-v0_central = Broker.from_config(load_config_central(f'{name}/{name}.yml', name))
-v1_central = from_config(load_config_central(f'{name}/{name}.yml', name))
-central = from_config(load_config_central(f'{name}/{name}.yml', name)).v2
